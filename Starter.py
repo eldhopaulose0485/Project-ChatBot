1e82a77709d70cd83c9dc01391e2b36e1bf89054
vv-2001 <vishnuvinod2772001@gmail.com>
2020-06-13 01:09:35 +0530

    get time

--- Starter.py
+++ Starter.py
@@ -1,4 +1,19 @@
 #
 #   Project Chat-Bot
 #
-print("Hello User, How can i help You")
+
+import datetime as dt
+
+def init():
+    #udata = open('userdata.cbdb', 'w')
+    print('############################')
+    print('    Welcome To Chat-Bot')
+    print('############################')
+    #udata.write('Chat-Bot Database\n')
+    cloak = str(dt.datetime.now()).split(' ')
+    cloak = str(cloak[1]).split(':')
+    if int(cloak[0]) < 12:
+        print('Good Morning ')
+    #udata.close()
+
+init()
