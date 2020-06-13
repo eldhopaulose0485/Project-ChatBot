3eef57ee39b02cad9d3c3337909ee04c5b3f2260
vv-2001 <vishnuvinod2772001@gmail.com>
2020-06-14 00:36:00 +0530

    added greetting

--- Starter.py
+++ Starter.py
@@ -4,16 +4,22 @@
 
 import datetime as dt
 
+
 def init():
-    #udata = open('userdata.cbdb', 'w')
+    # udata = open('userdata.cbdb', 'w')
     print('############################')
     print('    Welcome To Chat-Bot')
     print('############################')
-    #udata.write('Chat-Bot Database\n')
+    # udata.write('Chat-Bot Database\n')
     cloak = str(dt.datetime.now()).split(' ')
     cloak = str(cloak[1]).split(':')
     if int(cloak[0]) < 12:
-        print('Good Morning ')
-    #udata.close()
+        print('Good Morning, I am your virtual assistant')
+    else:
+        print('Good Afternoon, I am your virtual assistant')
+    print('How can i helpyou today?')
+    response = input()
+    # udata.close()
+
 
 init()
