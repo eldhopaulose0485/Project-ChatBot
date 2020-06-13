de7bffafccce488a9f81def14e391271aac2c17f
TheVV <vishnuvinod2772001@gmail.com>
2020-06-13 04:55:23 -0400

    added i am...

--- Starter.py
+++ Starter.py
@@ -6,9 +6,9 @@ import datetime as dt
 
 def init():
     #udata = open('userdata.cbdb', 'w')
-    print('############################')
-    print('    Welcome To Chat-Bot')
-    print('############################')
+    print('####################################')
+    print('    Welcome To Tourism Helpline')
+    print('####################################')
     #udata.write('Chat-Bot Database\n')
     cloak = str(dt.datetime.now()).split(' ')
     cloak = str(cloak[1]).split(':')
