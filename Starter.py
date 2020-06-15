d06460084ec60dd6ca0a466422f549f3fc2c340c
the-vv <vishnuvinod2772001@gmail.com>
2020-06-16 00:34:45 +0530

    testing audio and added webview

--- Starter.py
+++ Starter.py
@@ -3,6 +3,19 @@
 #
 
 import datetime as dt
+from googlesearch import search
+import webview
+import pyttsx3
+import os
+
+
+
+
+def speak(word):
+    engine = pyttsx3.init()
+    engine.say(word)
+    engine.run()
+
 
 def init():
     print('\n############################')
@@ -15,8 +28,14 @@ def init():
     else:
         print('Good Afternoon, I am your virtual Tour Guide')
     print('Where do you want to explore today?')
+    speak('Where do you want to explore today?')
     response = input()
-    
+    links = list(search('about ' + response, tld="co.in", num=10, stop=10, pause=2))
+    # for j in links: 
+    #     print(j)
+    print(links[0])
+    webview.create_window('Hello world', links[0])
+    webview.start()
 
 
 init()
